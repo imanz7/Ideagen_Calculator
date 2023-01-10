ec95071c19ad23f12f297b668c8663d0056a3433
Iman Zahurin <imanzh7@gmail.com>
2023-01-10 10:07:03 +0800

    Add project files.

---
+++ WindowsFormsApp1/Calc.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Calc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Calculate = new System.Windows.Forms.Button();
+            this.Reset = new System.Windows.Forms.Button();
+            this.Input = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(136, 167);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Please enter value";
+            // 
+            // Calculate
+            // 
+            this.Calculate.Location = new System.Drawing.Point(130, 288);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(155, 46);
+            this.Calculate.TabIndex = 1;
+            this.Calculate.Text = "Calculate";
+            this.Calculate.UseVisualStyleBackColor = true;
+            this.Calculate.Click += new System.EventHandler(this.Calculate_Click);
+            // 
+            // Reset
+            // 
+            this.Reset.Location = new System.Drawing.Point(130, 340);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(155, 46);
+            this.Reset.TabIndex = 2;
+            this.Reset.Text = "Reset";
+            this.Reset.UseVisualStyleBackColor = true;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(130, 203);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(155, 26);
+            this.Input.TabIndex = 3;
+            // 
+            // Calc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(422, 524);
+            this.Controls.Add(this.Input);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.Calculate);
+            this.Controls.Add(this.label1);
+            this.Name = "Calc";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Calculate;
+        private System.Windows.Forms.Button Reset;
+        private System.Windows.Forms.TextBox Input;
+    }
+}
+
---
+++ WindowsFormsApp1/Calc.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace WindowsFormsApp1
+{
+    public partial class Calc : Form
+    {
+        public string sum;
+        public string operation;
+        public string[] number;
+        public int sort;
+        public string total;
+        public string errorDetail;
+
+        public Calc()
+        {
+            InitializeComponent();
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            Input.Text = " ";
+        }
+
+        private void Calculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                errorDetail = null;
+                sum = Input.Text;
+
+                //var acceptableOperators = "+ - x / ( )".Split();
+                //if (!acceptableOperators.Contains(sum))
+                if (sum.Contains(" "))
+                {
+                    // test regex  
+                    string[] digits = Regex.Split(sum, @"\s*([-+/*()])\s*");
+                    List<String> tag = new List<string>(digits);
+                    //List<String> tag = new List<string>(sum.Split(' '));
+
+                    tag.RemoveAll(s => string.IsNullOrWhiteSpace(s));
+
+                    var bracket = tag.Where(stringToCheck => stringToCheck.Contains("(")).Count();
+                    Console.WriteLine("count: " + bracket);
+
+                    while (tag.Contains("("))
+                    {
+                        tag = FirstBracket(tag);
+                    }
+
+                    while (tag.Contains("*") || tag.Contains("/"))
+                    {
+                        tag = MulDiv(tag);
+                    }
+
+                    tag = AddSub(tag);
+
+                    Console.WriteLine("-----------");
+                    total = string.Concat(tag);
+
+                    if (errorDetail != null)
+                        MessageBox.Show("Input cannot be 0. \nError: ");
+                    else
+                        MessageBox.Show(total);
+
+                }
+                else
+                    MessageBox.Show("Please ensure numbers, operators and brackets are all separated by spaces");
+            }
+            
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private List<String> FirstBracket(List<String> tag)
+        {
+            List<String> sorted = new List<string>();
+            var index = tag.FindLastIndex(a => a == "(");
+            var start = index;
+            var stop = tag.FindIndex(a => a == ")");
+            var range = stop - index + 1;
+
+            /*List<string> sorted = tag.SkipWhile((value, ind) => ind < index && ind < stop).ToList();
+            Console.WriteLine("*******");
+            foreach (string amount in sorted)
+            {
+                Console.WriteLine(amount);
+            }*/
+            
+            while (index <= stop)
+            {
+                sorted.Add(tag[index]);
+                index++;
+            }
+
+            tag.RemoveRange(start, range);                  
+
+            while (sorted.Contains("*") || sorted.Contains("/"))
+            {
+                sorted = MulDiv(sorted);
+            }
+            
+            sorted = AddSub(sorted);
+            tag.Insert(start, sorted[0]);
+            return tag;
+        }
+
+        private List<String> MulDiv(List<String> sorted)
+        {
+            for(var i = 0; i < sorted.Count; i++)
+            {
+                if (sorted[i] == "*")
+                {
+                    var temp = Convert.ToDouble(sorted[i - 1]) * Convert.ToDouble(sorted[i + 1]);
+                    sorted[i - 1] = temp.ToString();
+                    sorted.RemoveRange(i, 2);
+                    i = 0;
+                }
+
+                else if (sorted[i] == "/")
+                {
+                    if(Convert.ToDouble(sorted[i + 1]) != 0)
+                    {
+                        var temp = Convert.ToDouble(sorted[i - 1]) / Convert.ToDouble(sorted[i + 1]);
+                        sorted[i - 1] = temp.ToString();
+                        sorted.RemoveRange(i, 2);
+                        i = 0;
+                    }
+                    else
+                    {
+                        errorDetail = "Input cannot be 0. \nError: ";
+                        sorted.Clear();
+                    }
+                    
+                }
+                else if (sorted[i] == ")" || sorted[i] == "(")
+                    sorted.RemoveRange(i, 1);              
+            }
+
+            return sorted;
+        }
+
+        private List<String> AddSub(List<String> sorted)
+        {
+            for (var i = 0; i < sorted.Count; i++)
+            {
+                if (sorted[i] == "+")
+                {
+                    var temp = Convert.ToDouble(sorted[i - 1]) + Convert.ToDouble(sorted[i + 1]);
+                    sorted[i - 1] = temp.ToString();
+                    sorted.RemoveRange(i, 2);
+                    i = 0;
+                }
+
+                else if (sorted[i] == "-")
+                {
+                    var temp = Convert.ToDouble(sorted[i - 1]) - Convert.ToDouble(sorted[i + 1]);
+                    sorted[i - 1] = temp.ToString();
+                    sorted.RemoveRange(i, 2);
+                    i = 0;
+                }
+                else if (sorted[i] == ")" || sorted[i] == "(")
+                    sorted.RemoveRange(i, 1);
+            }          
+            
+            return sorted;
+        }
+    }
+}
